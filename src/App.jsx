f8f69f40d8528ac9b77c03431675453bb7448dbe
asib ahmed <ahmedasib1@gmail.com>
2023-11-30 00:07:02 +0600

    show data

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,40 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [user, setUser] = useState([]);
+  const [users, setUsers] = useState([]);
   useEffect(()=>{
-    fetch('http://localhost:5000/user')
+    fetch('http://localhost:5000/users')
     .then(res => res.json())
-    .then(data => setUser(data))
+    .then(data => setUsers(data))
   }
-  ,[])
+  ,[]);
+
+  const handlesubmit = event =>{
+      event.preventDefault();
+      const form = event.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const user = {name, email};
+      console.log(user)
+  }
+
+
   return (
+    
     <>
 
       <h1>User Management</h1>
-      <h3>toal user: {user.length}</h3>
+      <form onSubmit={handlesubmit}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+      <h3>toal user: {users.length}</h3>
       <div>
         {
-
+          users.map(u => <p key={u.id}>{u.name}: {u.email}</p>)
         }
       </div>
     </>
